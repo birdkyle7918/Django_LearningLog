0f13006b8d4f7f580c93361cab411b83a3d06ced
xbk2000 <823972629@qq.com>
2019-12-29 10:49:15 +0800

    The Terminal

---
+++ learning_logs/urls.py
@@ -0,0 +1,37 @@
+"""定义learning_logs的URL"""
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+    # 主页
+    url(r'^$', views.index, name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # 显示特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # 用于添加新主题的网页界面
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # 用于添加新条目的界面
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    # 用于编辑条目的界面
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
+
+    # 用于删除条目的界面
+    url(r'^delete_entry/(?P<entry_id>\d+)/$', views.delete_entry, name='delete_entry'),
+
+    # 用于确认删除条目
+    url(r'delete_entry_confirm/(?P<entry_id>\d+)/$', views.delete_entry_confirm, name='delete_entry_confirm'),
+
+    # 用于删除主题
+    url(r'delete_topic/(?P<topic_id>\d+)/$', views.delete_topic, name='delete_topic'),
+
+    # 用于确认删除主题
+    url(r'delete_topic_confirm/(?P<topic_id>\d+)/$', views.delete_topic_confirm, name='delete_topic_confirm')
+
+]
---
+++ learning_logs/views.py
@@ -0,0 +1,123 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
+
+# Create your views here.
+
+
+def index(request):
+    # 学习笔记的主页
+    return render(request, 'learning_logs/index.html')
+
+
+@login_required
+def topics(request):
+    # 显示所有的主题
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    # 把数据库中该用户所有的topics全都拿出来作为上下文
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+@login_required
+def topic(requrst, topic_id):
+    # 显示单个主题和其所有条目
+    topic = Topic.objects.get(id=topic_id)
+    # 确认请求的topic属于当前用户
+    if topic.owner != requrst.user:
+        raise Http404
+    entries = topic.entry_set.order_by('-date_added')    # -号指的是降序排序
+    context = {'topic': topic, 'entries': entries}
+    return render(requrst, 'learning_logs/topic.html', context)
+
+
+@login_required
+def new_topic(request):
+    # 添加新主题
+    if request.method != 'POST':
+        # 未提交数据，创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)  # 第一个参数是请求类型，第二个是模板，最后一个是数据
+
+
+@login_required
+def new_entry(request, topic_id):
+    # 在特定的主题添加新的条目
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个新的表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+@login_required
+def edit_entry(request, entry_id):
+    # 编辑已有的条目
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+    if request.method != 'POST':
+        # 初次请求，用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+@login_required
+def delete_entry(request, entry_id):
+    entry_to_be_deleted_id = entry_id
+    entry_tobe_deleted = Entry.objects.get(id=entry_id)
+    topic = entry_tobe_deleted.topic
+    context = {'entry_to_be_deleted_id': entry_to_be_deleted_id, 'topic': topic}
+    return render(request, 'learning_logs/delete_entry.html', context)
+
+
+@login_required
+def delete_entry_confirm(request, entry_id):
+    entry_tobe_deleted = Entry.objects.get(id=entry_id)
+    topic = entry_tobe_deleted.topic
+    entry_tobe_deleted.delete()
+    return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+
+def delete_topic(request, topic_id):
+    topic_to_be_deleted_id = topic_id
+    context = {'topic_to_be_deleted_id': topic_to_be_deleted_id}
+    return render(request, 'learning_logs/delete_topic.html', context)
+
+
+def delete_topic_confirm(request, topic_id):
+    topic_tobe_deleted = Topic.objects.get(id=topic_id)
+    topic_tobe_deleted.delete()
+    return HttpResponseRedirect(reverse('learning_logs:topics'))
