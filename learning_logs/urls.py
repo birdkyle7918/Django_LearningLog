f9f39d783b21fb114f7bb87e2042c2ac3982a026
xbk2000 <823972629@qq.com>
2020-01-15 14:25:43 +0800

    带有左侧菜单的版本

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
 
-    # 主页
+    # 引导页
     url(r'^$', views.index, name='index'),
 
-    # 显示所有的主题
-    url(r'^topics/$', views.topics, name='topics'),
+    # 预览页
+    url(r'^overall/$', views.overall, name='overall'),
 
-    # 显示特定主题的详细页面
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    # 主页
+    url(r'^main/(?P<topic_id>\d+)$', views.main, name='main'),
 
     # 用于添加新主题的网页界面
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -15,24 +15,22 @@ def index(request):
 
 
 @login_required
-def topics(request):
-    # 显示所有的主题
+def overall(request):
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    # 把数据库中该用户所有的topics全都拿出来作为上下文
     context = {'topics': topics}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/overall.html', context)
 
 
 @login_required
-def topic(requrst, topic_id):
-    # 显示单个主题和其所有条目
-    topic = Topic.objects.get(id=topic_id)
-    # 确认请求的topic属于当前用户
-    if topic.owner != requrst.user:
+def main(request, topic_id):
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    topic_to_view = Topic.objects.get(id=topic_id)
+    if topic_to_view.owner != request.user:
         raise Http404
-    entries = topic.entry_set.order_by('-date_added')    # -号指的是降序排序
-    context = {'topic': topic, 'entries': entries}
-    return render(requrst, 'learning_logs/topic.html', context)
+    entries = topic_to_view.entry_set.order_by('-date_added')    # -号指的是降序排序，笔记按照降序排序
+
+    context = {'topics': topics, 'topic_id': topic_id, 'entries': entries, 'topic_to_view':topic_to_view}
+    return render(request, 'learning_logs/main.html', context)
 
 
 @login_required
@@ -48,7 +46,7 @@ def new_topic(request):
             new_topic = form.save(commit=False)
             new_topic.owner = request.user
             new_topic.save()
-            return HttpResponseRedirect(reverse('learning_logs:topics'))
+            return HttpResponseRedirect(reverse('learning_logs:overall'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)  # 第一个参数是请求类型，第二个是模板，最后一个是数据
 
@@ -68,7 +66,7 @@ def new_entry(request, topic_id):
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+            return HttpResponseRedirect(reverse('learning_logs:main', args=[topic_id]))
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
@@ -88,7 +86,7 @@ def edit_entry(request, entry_id):
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+            return HttpResponseRedirect(reverse('learning_logs:main', args=[topic.id]))
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
@@ -108,16 +106,17 @@ def delete_entry_confirm(request, entry_id):
     entry_tobe_deleted = Entry.objects.get(id=entry_id)
     topic = entry_tobe_deleted.topic
     entry_tobe_deleted.delete()
-    return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    return HttpResponseRedirect(reverse('learning_logs:main', args=[topic.id]))
 
 
 def delete_topic(request, topic_id):
     topic_to_be_deleted_id = topic_id
-    context = {'topic_to_be_deleted_id': topic_to_be_deleted_id}
+    topic_to_be_deleted = Topic.objects.get(id=topic_to_be_deleted_id)
+    context = {'topic_to_be_deleted_id': topic_to_be_deleted_id, 'topic_to_be_deleted': topic_to_be_deleted}
     return render(request, 'learning_logs/delete_topic.html', context)
 
 
 def delete_topic_confirm(request, topic_id):
     topic_tobe_deleted = Topic.objects.get(id=topic_id)
     topic_tobe_deleted.delete()
-    return HttpResponseRedirect(reverse('learning_logs:topics'))
+    return HttpResponseRedirect(reverse('learning_logs:overall'))
